421925d17e994d30f6cf5ffb75880a3ff4880bc3
bitucosouza <bitucosouza@gmail.com>
2025-09-12 18:46:57 +0000

    Adicionando o backup do banco

---
+++ database/BKP_BD_ficheiro.bat
@@ -0,0 +1,27 @@
+﻿@echo off
+rem (Nome do Usuário do banco para realizar o backup)
+REM nome pasta de backup
+REM nome pasta de instalação do PostgreSQL se diferente de C:\Arquivos de programas\PostgreSQL\8.4\
+REM
+REM (Nome do usuário do PostgreSQL que executará o script)
+SET PGUSER=postgres
+rem (Senha do usuário acima)
+SET PGPASSWORD=postgres
+rem (Nome do banco)
+SET BD=bd_ficheiro
+rem (Indo para a raiz do disco)
+chdir C:\Users\06375071771\Desktop\Backup SIGSocial
+rem (Selecionando a pasta onde será realizada o backup)
+chdir C:\Users\06375071771\Desktop\Backup SIGSocial
+
+echo "Aguarde, realizando o backup do Banco de Dados"
+rem Observação: Caso queira colocar o nome do backup seguindo de uma data é só usar:
+for /f "tokens=1,2,3,4 delims=/ " %%a in ('DATE /T') do set Date=%%a-%%b-%%c
+rem O comando acima serve para armazenar a data no formato dia-mes-ano na variável Date;
+
+C:\Users\06375071771\Desktop\postgresql-17.5-3-windows-x64-binaries\pgsql\bin\pg_dump.exe -h localhost -p 5432 -U postgres -Fc -b -v -f "C:\Users\06375071771\Desktop\Backup SIGSocial\Backup SISVoga %BD%_%Date%.backup" "%BD%"  
+
+pause
+      
+rem (sair da tela depois do backup)
+rem exit
---
+++ database/RESTORE_BD_SIGSocial_teste.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+REM COLOCAR USUARIO E SENHA DO BANCO A SER RESTAURADO
+SET BD=BD_Sigsocial
+SET PGUSER=usr_desenv
+SET PGPASSWORD=Marinha@2025
+REM ALTERAR O DIRETÓRIO EM QUE SE ENCONTRA O ARQUIVO A SER RESTAURADO
+
+chdir C:\Users\06375071771\Desktop\Backup SIGSoscial\Backups
+
+REM !!!ATENÇÃO: ANTES DE RESTAURAR TABELAS OU SCHEMAS, VERIFIQUE SE ESTES OBJETOS POSSUEM TABLESPACES.
+REM CASO POSSUA, CRIE AS TABLESPACES.
+REM !!!ATENÇÃO: MUDAR NOME DO ARQUIVO ANTES DE EFETUAR O BACKUP
+ 
+
+REM - U usuario, -d Banco, -c apaga objetos antes de cria-los, -v imprime mensagens de acoes a serem executadas 
+"C:\Users\06375071771\Desktop\postgresql-17.5-3-windows-x64-binaries\pgsql\bin\pg_restore.exe"  -h 10.5.112.244 -p 32011 -U usr_desenv -d "%BD%" -Fc -v -a "C:\Users\06375071771\Desktop\Backup SIGSocial\Backup SISVoga BD_Sigsocial_08-08-2025.backup"
+
+BD_Sigsocial
+
+pause
+rem (sair da tela depois do restore)
+exit pg_restore.exe
