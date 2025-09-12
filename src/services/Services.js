5b1dd2f24bbecb703cefb74e678f49ec56f42b3d
Thiago Souza <bitucosouza@gmail.com>
2025-09-11 23:17:41 -0300

    finalizado o CRUD de usuários

--- src/services/Services.js
+++ src/services/Services.js
@@ -12,6 +12,10 @@ class Services {
     async criaRegistro(dadosDoRegistro) {
         return dataSource[this.model].create(dadosDoRegistro);
     }
+    async atualizaRegistro(dadosNovos, id) {
+        return dataSource[this.model].update(dadosNovos, { where: { id: id } });
+    }
+    
     async deletaRegistro(id) {
         return dataSource[this.model].destroy({ where: { id: id } });
     }
